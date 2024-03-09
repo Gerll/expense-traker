04cabbc4e969c570fda8728e1a2597ef09e91e0e
gerll <gerll.bm@hotmail.com>
2024-03-08 21:08:41 -0800

    Added tailwind and some style

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,17 @@ import IncomeExpenses from "./components/IncomeExpenses";
 function App() {
   return (
     <GlobalProvider>
-      <Header />
-      <IncomeExpenses />
-      <Balance />
-      <TransactionForm />
-      <TransactionList />
+      <div className="bg-zinc-950 text-white h-screen flex justify-center items-center">
+        <div className="bg-zinc-800 p-10 rounded-lg flex">
+          <div className="">
+            <Header />
+            <IncomeExpenses />
+            <Balance />
+            <TransactionForm />
+          </div>
+          <TransactionList />
+        </div>
+      </div>
     </GlobalProvider>
   );
 }
--- src/components/transactions/TransactionForm.jsx
+++ src/components/transactions/TransactionForm.jsx
@@ -21,9 +21,11 @@ function TransactionForm() {
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input type="text" placeholder='Enter a Description' onChange={(e) => setDescription(e.target.value)}/>
-                <input type="number" step={0.01} placeholder='00.00' onChange={(e) => setAmount(e.target.value)}/>
-                <button>
+                <input type="text" placeholder='Enter a Description' onChange={(e) => setDescription(e.target.value)}
+                    className='bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 w-full'/>
+                <input type="number" step={0.01} placeholder='00.00' onChange={(e) => setAmount(e.target.value)}
+                    className='bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 w-full'/>
+                <button className='bg-indigo-700 text-white px-3 py-2 rounded-lg block mb-2 w-full'>
                     Add Transaction
                 </button>
             </form>
