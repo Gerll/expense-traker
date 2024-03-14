6d0af5cf592fd25aba04cdaa7acdf49420a8f70e
gerll <gerll.bm@hotmail.com>
2024-03-14 14:52:34 -0700

    Clean the form after use

--- src/components/transactions/TransactionForm.jsx
+++ src/components/transactions/TransactionForm.jsx
@@ -5,7 +5,7 @@ import {useGlobalState} from '../../context/GlobalState'
 
 function TransactionForm() {
     const {addTransaction} = useGlobalState()
-    const [description, setDescription] = useState()
+    const [description, setDescription] = useState('')
     const [amount, setAmount] = useState(0)
 
     const onSubmit = (e) => {
@@ -15,15 +15,17 @@ function TransactionForm() {
             description,
             amount: +amount
         })
+        setAmount(0);
+        setDescription('');
         console.log(description,amount)
     }
 
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input type="text" placeholder='Enter a Description' onChange={(e) => setDescription(e.target.value)}
+                <input type="text" placeholder='Enter a Description' onChange={(e) => setDescription(e.target.value)} value={description}
                     className='bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 w-full'/>
-                <input type="number" step={0.01} placeholder='00.00' onChange={(e) => setAmount(e.target.value)}
+                <input type="number" step={0.01} placeholder='00.00' onChange={(e) => setAmount(e.target.value)} value={amount}
                     className='bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 w-full'/>
                 <button className='bg-indigo-700 text-white px-3 py-2 rounded-lg block mb-2 w-full'>
                     Add Transaction
